08f1bed2daaf43b8d6e5291f9825150839c0499e
Apple <apple@Apples-MacBook-Pro.local>
2021-01-23 10:37:42 +0530

    latest code

--- src/AppUtils/Mutations.js
+++ src/AppUtils/Mutations.js
@@ -1,71 +1,26 @@
-// const Mutations = {
-// }
-// export default Mutations;
-const Mutations = (api, queryBody, requestType) => {
-    console.log(api, queryBody, requestType);
-    let isFormData = false;
-    // setLoaderData(true);
-    if (queryBody) {
-        if (queryBody instanceof FormData) {
-            isFormData = true;
-        }
-    }
-    const requestOptions = {
-        method: requestType ? requestType.toUpperCase() : queryBody ? 'POST' : 'GET',
-        headers: {
-
-        },
-
-        // body: isFormData ? queryBody : JSON.stringify(queryBody),
-    };
-
-    if (!isFormData) {
-        requestOptions.headers['Content-Type'] = 'application/json';
-    }
-
-    return fetch(api, requestOptions)
-        .then((response) => response.json()
-            .then((response) => {
-                // console.log('eresponse', response.message);
-                // setLoaderData(false);
-                if ((response.status && response.status !== 200 && response.status !== 201) || response.errors) {
-                    throw new Error(JSON.stringify(response));
-                } else {
-                    if (response.message) {
-                        alert(response.message);
-                    }
-                    return Promise.resolve(response);
-                }
-            }))
-        .catch((error) => {
-            console.log('error for api', error);
-            // setLoaderData(false);
-            if (error.message === 'Failed to fetch') {
-                alert('Please check your Internet connection');
-            } else {
-                let errMsg = typeof error.message === 'string' ? error.message : JSON.parse(error.message);
-                errMsg = errMsg.message ? errMsg.message : (typeof errMsg === 'string' ? errMsg : JSON.parse(errMsg));
-                switch (+errMsg.status) {
-                    case 422: {
-                        for (let index = 0; index < errMsg.errors.length; index++) {
-                            const element = errMsg.errors[index];
-                            alert(element.msg);
-                        }
-                        break;
-                    }
-                    case 400: {
-                        alert(errMsg.message);
-                        break;
-                    }
-                    case 401: {
-                        alert(errMsg.message);
-                        break;
-                    }
-                    default: {
-                        alert('Sorry something went wrong');
-                    }
-                }
+const Mutations = {
+    login: async function (loginData) {
+        return fetch(
+            "https://projects.yellowsoft.in/new_trend/app/login",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(loginData)
             }
-        });
-};
+        )
+            .then(
+                response => {
+                    // console.log(response, "====")
+                    return response.json();
+                },
+
+            )
+            .catch(error => {
+                console.log("Error: ", error);
+            });
+    },
+}
 export default Mutations;
+
--- src/containers/Tabs/SlidingUpPanel.js
+++ src/containers/Tabs/SlidingUpPanel.js
@@ -1,22 +1,50 @@
 import React, { useEffect } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
+import AppUtils from '@AppUtils'
 import { Mutations } from '@AppUtils'
 
 export default function SlidingUpPanel() {
     useEffect(() => {
+        let numbers = [{
+            id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+            title: 'First Item',
+        },
+        {
+            id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+            title: 'Second Item',
+        },
+        {
+            id: '58694a0f-3da1-471f-bd96-145571e29d72',
+            title: 'Third Item',
+        }];
+        // console.table(numbers);
         fetchData()
     }, [])
     function fetchData() {
-        var value = 20
-        Mutations('https://randomuser.me/api/?&results=' + value, null, 'GET')
+        var value = 1
+        AppUtils('https://projects.yellowsoft.in/new_trend/app/members/' + value)
             .then((response) => {
 
-                console.log("kjkhjkhf", response)
+                console.table(response)
             });
     }
+    function onLogin() {
+        var data = {
+            "email": "test@gmail.com",
+            "password": "123456"
+        }
+        AppUtils('https://projects.yellowsoft.in/new_trend/app/login', data, "POST")
+            .then((response) => {
+
+                console.log("logginnn", response)
+            });
+    }
+
     return (
         <View>
-            <Text>SlidingUpPanel</Text>
+            <TouchableOpacity onPress={onLogin}>
+                <Text>SlidingUpPanel</Text>
+            </TouchableOpacity>
         </View>
     )
 }
